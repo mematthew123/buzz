a34813656537652482d6203162702f900c80f192
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-05-17 13:09:37 -0600

    updated single product page styling

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -43,39 +43,48 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return { paths, fallback: "blocking" };
 };
-
 const ProductPage: React.FC<{ product: Product }> = ({ product }) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    return <div>Loading...</div>; // You can customize this as per your needs
+    return <div>Loading...</div>; 
   }
 
   return (
-    <div className="container mx-auto px-6 py-8 h-[400px] w-[550px]">
-      <h1>{product.title}</h1>
-
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        {product.imageUrls.map((url, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={url}
-              alt={product.title}
-              className="h-[400px] w-[550px] aspect-auto"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      {/* Rest of the product details */}
+    <div className="container mx-auto px-6 py-8">
+      <div className="flex flex-wrap -mx-4">
+        <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+            className="shadow-lg rounded-lg overflow-hidden h-96"
+          >
+            {product.imageUrls.map((url, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src={url}
+                  alt={product.title}
+                  className="h-full w-full object-cover"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="w-full md:w-1/2 px-4">
+          <h1 className="text-4xl font-bold mb-4">{product.title}</h1>
+          <p className="text-gray-600 mb-4">{product.description}</p>
+          <p className="text-gray-600"><strong>Type:</strong> {product.type}</p>
+          <p className="text-gray-600"><strong>THC:</strong> {product.thc}%</p>
+          <p className="text-gray-600"><strong>CBD:</strong> {product.cbd}%</p>
+          <p className="text-gray-600"><strong>Price:</strong> ${product.price}</p>
+          <p className="text-gray-600"><strong>Size:</strong> {product.size}</p>
+        </div>
+      </div>
     </div>
   );
 };
