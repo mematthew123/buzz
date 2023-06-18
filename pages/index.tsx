a577d4449a1b3caa27ee53542ea7f21915007831
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-06-18 14:26:28 -0600

    all sections float

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ import Featured from "@/components/Featured";
 import Head from "next/head";
 import Layout from "@/components/Layout";
 import Banner from "@/components/Banner";
+import CTARight from "@/components/CTARight";
 
 const inter = Fraunces({
   subsets: ["latin"],
@@ -52,41 +53,43 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const Home: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
   featuredProduct,
-  featuredData // add the featured product prop here
+  featuredData, // add the featured product prop here
 }) => {
   return (
     <>
-    <Head>
-        <title>Kootanei Organics | Organic small batch cannabis in western Montana</title>
-        <meta name="description" content="Discover Kootanei Organics and our range of organic small batch cannabis products grown in the heart of western Montana. Shop for vapes, pre-rolls, edibles and more." />
+      <Head>
+        <title>
+          Kootanei Organics | Organic small batch cannabis in western Montana
+        </title>
+        <meta
+          name="description"
+          content="Discover Kootanei Organics and our range of organic small batch cannabis products grown in the heart of western Montana. Shop for vapes, pre-rolls, edibles and more."
+        />
       </Head>
       <div>
         <Navbar />
         <Layout>
-          <Banner/>
-        <div className="max-w-[1240px] h-[100vh] m-auto flex justify-between items-center p-4 text-white">
-          <div className="text-gray-50 absolute inset-0 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+          <Banner />
+          {/* Hero section */}
+{/* Hero section */}
+<div className="hero bg-[#FAEDCD] border border-gray-200 shadow-lg max-w-[1240px] h-[80vh] m-auto flex flex-col justify-center items-center p-4 rounded-lg">
+  <div className="hero-content flex flex-col justify-center items-center text-center sm:items-start space-y-6">
+    <h1 className={inter.className + " text-7xl font-extrabold text-gray-800"}>
+      Kootanei Organics
+    </h1>
+    <p className="text-xl font-light text-gray-600">
+      Organic small batch cannabis grown in the heart of western Montana
+    </p>
+    <Link href="/menu">
+      <p className="inline-block px-10 py-3 text-base font-medium text-white bg-green-800 rounded-lg hover:bg-green-700 transition-colors duration-200 shadow-md hover:shadow-lg">
+        Shop Now
+      </p>
+    </Link>
+  </div>
+</div>
 
-            <h1
-              className={
-                inter.className +
-                " text-6xl font-bold text-gray-600 text-center underline drop-shadow-lg shadow-black"
-              }
-            >
-              Kootanei Organics
-            </h1>
-            <p className=" mt-4 text-gray-600  flex font-extrabold  text-center align-middle left-50% top-50%    mb-6 drop-shadow-lg shadow-black">
-              Organic small batch cannabis grown in the heart of western Montana
-            </p>
-          </div>
-        </div>
-        <Link href="/menu">
-          <div className="flex items-center font-bold underline mb-24  text-xl border-b border-current gap-x-4 py-2 transition-all duration-300 group hover:pl-4 hover:pr-1">
-            <span>Shop Now</span>
-            <ArrowLongRightIcon className="w-5 h-5 group-hover:transform group-hover:translate-x-1 transition-all duration-300" />
-          </div>
-        </Link>
-        <div className=" flex lg:w-[1400px] bg-[#E9EDC9] mb-[80px] lg:mb-[150px] justify-center items-center text-center ">
+
+          {/* <div className=" flex lg:w-[1400px] bg-[#E9EDC9] mb-[80px] lg:mb-[150px] justify-center items-center text-center ">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
             <FeatureSection
               initial={{ opacity: 0, x: -50 }}
@@ -113,15 +116,17 @@ const Home: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
               description="Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat."
             />
           </div>
-        </div>
-        <MenuBoard specials={[]} />
-        <FeaturedProduct product={featuredProduct} />
-        <Content />
-        <Featured featuredData={featuredData} />
-        <WhyUs />
-        <Testimonials />
-   
-        <Footer />
+        </div> */}
+          <Featured featuredData={featuredData} />
+
+          <MenuBoard specials={[]} />
+          {/* <FeaturedProduct product={featuredProduct} /> */}
+          <Content />
+          {/* <CTARight /> */}
+          <WhyUs />
+          <Testimonials />
+
+          <Footer />
         </Layout>
       </div>
     </>
